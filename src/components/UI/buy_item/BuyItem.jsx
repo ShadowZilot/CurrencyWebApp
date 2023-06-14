63baeb484b173bbb923128c0114201ffd104a0a9
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-06-14 19:36:37 +0300

    Change CSS with buy item

--- src/components/UI/buy_item/BuyItem.jsx
+++ src/components/UI/buy_item/BuyItem.jsx
@@ -5,9 +5,9 @@ const BuyItem = (props) => {
     return (
         <div className={cl.main_purchase_container}>
             <div className={cl.purchase_container}>
-                <h4>{props.purchase.amount}</h4>
-                <hr width={"100%"}/>
-                <h4>{props.purchase.amount_ruble}</h4>
+                <h4 className={cl.buy_item_text}>{props.purchase.amount}</h4>
+                <hr className={cl.item_divider}/>
+                <h4 className={cl.buy_item_text}>{props.purchase.amount_ruble}</h4>
             </div>
             <h4>{props.purchase.rate}</h4>
         </div>
