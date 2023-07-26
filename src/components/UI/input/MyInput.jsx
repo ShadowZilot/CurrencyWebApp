ad72da66b897981247c90a7221f70d13d368d7de
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-07-26 22:52:52 +0300

    Решён баг с запятой

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import classes from "./MyInput.module.css";
 
 const MyInput = ({children, ...props}) => {
+    function handleChange(event) {
+        event.target.value = event.target.value.replace(",", ".")
+        props.onChange(event)
+    }
+
     return (
-        <input className={classes.myInput} {...props} children={children}/>
+        <input className={classes.myInput} {...props}
+               onChange={handleChange}
+               children={children}/>
     );
 };
 
